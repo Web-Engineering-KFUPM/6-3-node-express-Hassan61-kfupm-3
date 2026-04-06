98329d1e05f4634cc91813344a8dbbe52dae1a3e
hassan12361 <49165616+hassan12361@users.noreply.github.com>
2026-04-06 10:20:55 +0300

    todo 6.1

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -11,12 +11,14 @@ import cors from "cors";
 app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
+import morgan from "morgan";
 
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
 
 
 // TODO 6.2: Create "/api/quote" route
-
+import { getRandomQuote } from "./quotes.js";
 
 // TODO 7: Start server using app.listen
